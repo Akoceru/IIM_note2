89189359e624daf0240a1a350d4c701baa2cad13
PandumWeb <lindalor.web@gmail.com>
2015-11-19 10:31:04 +0100

    add exam + grade + student to admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,7 @@ class AppKernel extends Kernel
             new \JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
