6240770a24325c82dc1c87d7d679828d0d498140
Akoceru <axl.78@hotmail.fr>
2015-11-24 15:42:05 +0100

    Final test(admin)

--- src/AppBundle/Tests/AdminControllerTest.php
+++ src/AppBundle/Tests/AdminControllerTest.php
@@ -12,6 +12,26 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AdminControllerTest extends WebTestCase
 {
+    public function testAdmin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('_submit')->form();
+
+        $form['_username'] = 'admin';
+        $form['_password'] = 'admin';
+
+
+
+        $client->submit($form);
+
+        $crawler = $client->request('GET', '/admin');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Admin', $client->getResponse()->getContent());
+    }
 
     public function testAdminAdd()
     {
@@ -31,10 +51,6 @@ class AdminControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/admin');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Admin', $client->getResponse()->getContent());
-
-
 
         $crawler = $client->request('POST', '/admin/admin');
         $form = $crawler->selectButton('Save')->form();
@@ -52,4 +68,33 @@ class AdminControllerTest extends WebTestCase
         $this->assertContains('admin2', $client->getResponse()->getContent());
     }
 
+    public function testAdminDelete()
+    {
+        $client = static::createClient();
+
+
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('_submit')->form();
+
+        $form['_username'] = 'admin';
+        $form['_password'] = 'admin';
+
+
+
+        $client->submit($form);
+
+        $crawler = $client->request('GET', '/admin');
+
+        $link = $crawler->filter('a.admin_delete')->eq(1)->link();
+
+
+        $client->click($link);
+
+
+        $client->request('GET', '/admin');
+
+
+        $this->assertContains('Admin', $client->getResponse()->getContent());
+    }
 }
